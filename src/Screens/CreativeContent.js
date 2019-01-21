e33ef57afa415abf7d4224ae24b72d8f13b8bb0c
Kayla Forsythe <kaylarobideau@gmail.com>
2019-01-21 17:53:04 -0600

    Update positioning

--- src/Screens/CreativeContent.js
+++ src/Screens/CreativeContent.js
@@ -2,8 +2,7 @@ import { html } from 'lit-html';
 
 export default function CreativeContent(state){
     return html`
-<div id="info">    
-    <div class = "aboutAuthor">
+    <div id="info" class="aboutAuthor"> 
         <img src="https://lh3.googleusercontent.com/k8p6PG-We9aMIb7LHvYKahHnMjRReQKZbF7qDuMiLp3hHErZfTM-2JwL4rxj95EfosMFxV_87GlFVMwMu9Hxx1CrYT_fy20GXf7orBtgLbnKkJZmLPW3fGbsfeI3DU3wOcTy4r-Nmkha4_N3eqEfB9MDTDoVQWQU-TUGIsnBE3g1reXnVWTfy1VaDU4kgHFDzAmWjDij15DpyInSqH8KFBmTd9TKkZPOz0pllhXe4hrFdprvs-QTg5gucPf44lYpVHZTQvnJG3pTtSG277QoXNP_kNB_H_FnwuOswFJRubryrZ5gBbqszXIIwzoN9rY33GCNJoeN-6VZi8MQG5EV2aEGJZCQNtIRRgdyiqTPMNKcCXu6r5yq6PsnGW1PMcysQC74sp_2F1AhClt3JYMd3ED5c5SBUyvyLrU9C778UaWFVgqu2yqBHbywFXE6QBkbNhA0n9y2qZfTkcTts28atftX3sI7x1qd2FpLKmU-yTKTh0U6wdRZeEcYbqx9umXBA9VyN6U3Z9nRy0-U0D5jNhxi7BFMIvOggTNMW_oK_nr4oAA1VApKuYwta6a-AJKBkt_d3cM_Yn38cyj-ate44M1aDtv04lVtCnMjj3E6x-qK6zEla-O8MC57v__ujWrhP7iood9JNoDb1vSa8_MBsrrpaw=w497-h400-no" 
             alt="Author" class="contain">
   
@@ -14,29 +13,30 @@ export default function CreativeContent(state){
         <p>Looking for more poetry? Stay tuned for more excerpts and releases from my new book</p><h2> Raw.</h2>
         <p>Along with other creative content pre- and post published.</p>   
     
+    
     </div>
 
-    <div class="coffee">
-        <div class="mapouter"><div class="gmap_canvas"><iframe width="400" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=books%2C%20coffee%2C%20coffee%20shop%2C&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.pureblack.de">pure black</a></div><style>.mapouter{text-align:right;height:300px;width:400px;}.gmap_canvas {overflow:hidden;background:none!important;height:300px;width:400px;}</style></div></div>
-    </div>
+<div id="coffee">
+
+<div class="mapouter"><div class="gmap_canvas"><iframe width="400" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=books%2C%20coffee%2C%20coffee%20shop%2C&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.pureblack.de">pure black</a></div><style>.mapouter{text-align:right;height:300px;width:400px;}.gmap_canvas {overflow:hidden;background:none!important;height:300px;width:400px;}</style></div></div>
+<h2>Coffee is my greatest motivater. Here are some local coffee shops to spark your inspiration.</h2>
 
 </div>
 
-<div id="Poems" class="slide">
+
+<div id="Poems">
 <h2>Raw</h2> <h3>Take a Peek...</h3>
 
-</div>
+    <div class="first">
+        <img src="https://lh3.googleusercontent.com/hvWdVsMJrDWMCQ3SnP1I7kuugqsXMIe2HT_xtzhqnhKQdK3dFY5cp7rOtv-QW27TVC9rLvi5gZNzhblDwMiD6Ef4kOCqYjL1b3NfP-g4DAN9Rul7V0henGEL1TqzfwMrE7XVKz6_B7PJJVuxJwqR6pwEZ9ZDeAlcJRiRje04xdRt4Rnjcoz5u2VYdwYA9zOLyNNJyLsgfqYIlx4fbXklJ2jUCPNf-0N1p9G6-3N9-VqH6qlEui6kU-Q9rkVjY_YtZY_apakIuPOHvdBiLYxjeklPadxjJ1h5ZwIFPHMI8P6IVhB8rPs8yLNAf4xl53MnOA2_QwuBc_cBhLTlUyzfNtRfi51ZABSmiXFbvnhw4C2d02JnZt1Svmc8-P68DfYrLfdOpKsiCTqTPVtncd_la_aVVdFArMBbfx3XV5pv4-umFVuDRqlkc9Onre3m7GSIllZ12E2yMZHk3-5AHPLmDGFAuZEEORJW3tL3-SDlzxpC-eke4cHimtqUfEtthgdc5X_pjyJhWbq2qXq3UtHdb_DcclCV9PDrHychjZ-zZoHSKFgl_bG8EYkUq5mpLLEboKfVILbglO7Mo_JLjmd89bABcBAf5ow=s661-w661-h657-no" alt="poem one" class="exerpts">
 
-<div id="p1" class="slide">
-</div>
+        <img src="https://lh3.googleusercontent.com/W7cRzVCfpIaR-j_jb1_H2WiLkTp1kyPBAgUzXqyqUcr0KRkhiCwSWOzlwBit_Krt7gP2vZBoKWvSiup7zF1mbpFuQYHBMHNEamhMQUeTON42VfGraVmyVlfB3rH_FZCbrOnSdTzd8_gZh8e3TQhJo64xXvJAqlhVTDzldRWMYC6mXJu4ZW1xndXAXlThle_r2LfSczbrEQM5098mZcGk3lR8cbmHvHQqq-QvbnPjjgGMTSLN5xfMcg07srM7iAaBAFidLVBkGfjbAB5s0lC3mLBhhS52mmOcTbc2_RVeGThBNScYuiLhfZ9VZG72Bb0oCLzb8T0IVX8mjmT-qLjV4t669A2umGCbx1ecCwh7gSxpf1JHjpRLQiNEL0JfCa4xRq58omSrjP8DRfCU02rjnpTmbQS-fn2xWX8mp8zCqhtV8YWQiRE-D0_j6CNZTCgnWlm9IiUiIPNWplyEuVO15zZaFZT4HiDyOINGgcwJoLmIqZW-1zDfX7e0hv3MgoR6kQp8-fei5bjnOjyZK9A_vPjpKVGzq_B9tuLlamWbw3264EoQsN7GrwX4Ui01U0nrF-1TXMpEnKisZKS-NxDNaXSYmUdcUnc=s657-w533-h657-no" alt="poem two" class="exerpts">
+    </div>
+    <div class="second">
+        <img src="https://lh3.googleusercontent.com/w0Ej1cZqAfu3oXyl83BNFJowEPGi2T8jxr-OXbrp3vWHRhcGBZH07WUsd7AWAAD2SgFzhJT4OKI_h8eW7ZRnV5vo_S_0exi0Vr6FS8_9mGty0_cUQ6hKSxI7IFKXeYvscJfanxYI7GM_7LDl_dXysX0ArlxN-zWN7KoSN4AuPDVvRoOLqKs44yJT31Y4vIrK7khI_4-GaJvesvYTf3XNmd4GlsdJj16sjbI2X4_L1cMy9LbuHJKWAuvIwEnQ5GqXT6ngVBZyTcXfQEQygAXUBUgI0HhvuXYhzH9UF19MIHn9B-9qpj_VLEwAt2i3Bt2MOMKRSEo7D7aw7U63F38HPFbI1qwgjqS9FA0jQfJunhlKqcuFeHTkr9JFCzQeEK49aqjhDGfb-NzDZ_Lwf7AMANoC7aF5OC_kV9-8wf_f_C7gC7vGlWQ1fu2rMYJVkhO1n3gq422jzGaCX7CvEHtP9ak0Ea4el-KfVmGhFGL1zrlxbyt_2ELzgXAGuy6IskdMLLBuX7dxNAB9k8TIopcHqcd7TEVBsDRaoo4Qad1TOFXWMez_rmY-_OWnZ-MA77QhnBDBwCZNJjq1S1ciE05an7PEs35epyY=s661-w661-h657-no" alt="poem three" class="exerpts">
 
-<div id="p2" class="slide">
-</div>
-
-<div id="p3" class="slide">
-</div>
-
-<div id="p4" class="slide">
+        <img src="https://lh3.googleusercontent.com/gOJgoEyHyTaNzYXJEBIwM4kVQXAFwD9m1V1Ugx6N2OyJFF3kwUlJConZCmLcy7tNYhI4Zt5kCzxtTuEPXFxVLqnxfHiMDnO5-uZ190ksDj01m7V7cNkgeFcHzdOtEAMYca42FHcl0jcbPHsmC0KjSXpmWErjX5sKIuvWCW39rehlt2uYuKOqPIabs3NfbwoXxtPk7rIgvDsbroajd2zZ6ydllf5a9vizfFZ3KDemqNxBrs7pLg8cIgRNHUci5lD1jq7H3nBLO31KmKKL4AbQciwMVlECqi17-sk98SPlpSIWw0PK2O-0cbbHm3Wy-_9dAzqA35-KyRU0hOmk_NAAqnQeGY9sT0csBcI7ddZW_OxuOJGQMChosELoCK51juC0L58xe37pnAjX4hslDkdO9sPVUCBV_dpjf7feSzWxVnQB-mgO6vKvBolEVypNaHuFeEQOchMhzZT_xI4V-kKA8XPa15MkM0nYruSPmeQsNzcwP2H3KUCuhHnc-deipamGl3MdY-6QIQZ6j533tvbvZrDnVZ7G08D9aSFFxJmGUEOqhBpIayzbGwK88rHkAJ-8lsHR6iQohZtl985DuZAjdCWKQAtxLGo=s667-w667-h657-no" alt="poem four" class="exerpts">
+    </div>
 </div>
 `;
 }
